2573041ed1014898968d0d2015cac3841266c79f
Hachana Saif <saifhachana@gmail.com>
2022-05-01 22:54:49 +0100

    firebase integrated

--- src/main/java/com/bprice/bpapipos/service/IStorageService.java
+++ src/main/java/com/bprice/bpapipos/service/IStorageService.java
@@ -4,8 +4,16 @@ import com.bprice.bpapipos.response.ResponseObject;
 import com.bprice.persistance.model.Storage;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
+
 public interface IStorageService {
-    public ResponseObject AddtoStorage(MultipartFile storage);
     public ResponseObject Deleteitem(String id);
     public ResponseObject finditembyid(String id);
-}
+    public String uploadFile(File file, String fileName) throws IOException;
+    public File convertToFile(MultipartFile multipartFile, String fileName);
+    public String getExtension(String fileName);
+    public ResponseObject upload(MultipartFile multipartFile);
+    public ResponseObject DeletePublicicteImage(String fileName) throws IOException;
+
+    }
