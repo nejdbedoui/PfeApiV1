a8c52c4799a529560a1062c28ee565c1efdd087a
Hachana Saif <saifhachana@gmail.com>
2022-05-05 21:53:51 +0100

    integration of storage documents

--- src/main/java/com/bprice/bpapipos/repository/IStorageRepository.java
+++ src/main/java/com/bprice/bpapipos/repository/IStorageRepository.java
@@ -4,7 +4,10 @@ package com.bprice.bpapipos.repository;
 import com.bprice.persistance.model.Storage;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface IStorageRepository extends MongoRepository<Storage,String> {
     public Storage findByIdStorage(String id);
+    public List<Storage> findAll();
 
 }
